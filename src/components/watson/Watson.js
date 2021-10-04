6afa66bc78ef80288af65c528304bfac72e837ac
Marcos26-tech <rm88267@fiap.com.br>
2021-10-04 17:14:44 -0300

    ajuste agenda

--- src/components/watson/Watson.js
+++ src/components/watson/Watson.js
@@ -1,13 +1,32 @@
-import React from 'react'
+// import React from 'react';
 
-function Watson() {
-    return (
-        <div>
+// export default () => {
+
+
+
+// window.watsonAssistantChatOptions = {
+//         integrationID: "138bf077-6b3c-4f60-abd2-f06d6b1a81f9", // The ID of this integration.
+//         region: "us-south", // The region your integration is hosted in.
+//         serviceInstanceID: "7542444f-0c10-47aa-afa0-fae9eeb13c60", // The ID of your service instance.
+//         onLoad: function(instance) { instance.render(); }
+//       };
+// setTimeout(function(){
+//       const t=document.createElement('script');
+//       t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" +
+//         (window.watsonAssistantChatOptions.clientVersion || 'latest') +
+//         "/WatsonAssistantChatEntry.js"
+//       document.head.appendChild(t);
+//     });
+ 
+// }              
+
+      
             
-        </div>
-    )
-}
+ 
+        
+   
 
-export default Watson
+
+
 
 
