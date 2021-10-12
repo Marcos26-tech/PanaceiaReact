885799a0c13822dc662d4d137c1572eaae2ce16f
Marcos26-tech <rm88267@fiap.com.br>
2021-10-11 22:57:57 -0300

    ajsute

--- src/pages/comunidade/PageComunidades.jsx
+++ src/pages/comunidade/PageComunidades.jsx
@@ -77,14 +77,14 @@ class PageComunidades extends Component {
 
           <Div>
             <img src={ImagemAutocuidado} alt="Autocuidado Feminino"/>
-            <p><a href={site} target="_blank" rel="noreferrer noopener"><span>"Autocuidado deve ser o cuidado consigo mesma, atenção às próprias necessidades do corpo, mentais e psicológicas." </span></a>
+            <p><a href={site} target="_blank" rel="noreferrer"><span>"Autocuidado deve ser o cuidado consigo mesma, atenção às próprias necessidades do corpo, mentais e psicológicas." </span></a>
               Esta definição é a mais simples e também mais abrangente. 
               O principal elemento do autocuidado é a atenção voltada para si mesmo, para as próprias necessidades e aspirações.
               O que envolve cuidar de todos os nossos principais aspectos como seres humanos: o físico, o mental e o emocional. </p>
           </Div>
           <Divi>
             <img src={ImagemSaude} alt="cuidados infantil" />
-            <p>No <a href={site1} target="_blank" rel="noreferrer noopener"><span>Outubro Rosa</span></a>,médicas refletem sobre como é preciso estruturar a rotina para cuidar da saúde e prevenir o câncer 
+            <p>No <a href={site1} target="_blank" rel="noreferrer"><span>Outubro Rosa</span></a>,médicas refletem sobre como é preciso estruturar a rotina para cuidar da saúde e prevenir o câncer 
               que mais afeta mulheres. Falar sobre a doença é fundamental! Para vencer o estigma, o receio, o peso que a cerca.
               Ao passar por um prédio iluminado de rosa durante este mês, faça uma reflexão. Se você é mulher, já fez seus exames de rotina este ano ?</p>
           </Divi>
