d01c592ec74eb12f51e15e518accc466241b80a4
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-09-12 09:27:51 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer";
-import Routes from "./routes";
+import Routes from "./Routes.jsx";
 
 const App = () => {
   return (
